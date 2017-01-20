736093950d31f71dc76220b4dbf6e90ba5f2fb5b
wendycwong <wendycwong@yahoo.com>
2017-01-20 14:56:50 -0800

    Added more print statement in pyunit_utlis.compare_frame.py to aid debugging.

--- h2o-py/tests/pyunit_utils/utilsPY.py
+++ h2o-py/tests/pyunit_utils/utilsPY.py
@@ -2669,9 +2669,10 @@ def compareOneNumericColumn(frame1, frame2, col_ind, rows, tolerance, numElement
 
         if not(math.isnan(val1)) and not(math.isnan(val2)): # both frames contain valid elements
             diff = abs(val1-val2)
-            assert diff <= tolerance, "failed frame values check! frame1 value = {0}, frame2 value =  {1}, " \
-                                      "at row {2}, column {3}.  The difference is {4}.".format(val1, val2, row_ind,
-                                                                                               col_ind, diff)
+            print("val1 is : {0} and val2 is {1}.  The difference is {2}\n".format(val1, val2, val1-val2))
+#            assert diff <= tolerance, "failed frame values check! frame1 value = {0}, frame2 value =  {1}, " \
+#                                      "at row {2}, column {3}.  The difference is {4}.".format(val1, val2, row_ind,
+#                                                                                               col_ind, diff)
         elif math.isnan(val1) and math.isnan(val2): # both frame contains missing values
             continue
         else:   # something is wrong, one frame got a missing value while the other is fine.
