7956e3d938d500793f71b0b54088e65bf66fc35e
wendycwong <wendycwong@yahoo.com>
2017-01-20 14:31:21 -0800

    Revert back to original time offsets.

--- h2o-parsers/h2o-orc-parser/src/main/java/water/parser/orc/OrcParser.java
+++ h2o-parsers/h2o-orc-parser/src/main/java/water/parser/orc/OrcParser.java
@@ -207,19 +207,19 @@ public class OrcParser extends Parser {
     boolean timestamp = columnType.equals("timestamp");
     long [] oneColumn = col.vector;
     if(col.isRepeating) {
-      long val = timestamp ? (oneColumn[0]/1000000-ADD_OFFSET) : correctTimeStamp(oneColumn[0]);
+      long val = timestamp ? oneColumn[0] / 1000000 : correctTimeStamp(oneColumn[0]);
       for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++)
         dout.addNumCol(cIdx, val, 0);
     } else if(col.noNulls) {
       for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++)
-        dout.addNumCol(cIdx, timestamp ? (oneColumn[rowIndex]/1000000-ADD_OFFSET): correctTimeStamp(oneColumn[rowIndex]), 0);
+        dout.addNumCol(cIdx, timestamp ? oneColumn[rowIndex] / 1000000 : correctTimeStamp(oneColumn[rowIndex]), 0);
     } else {
       boolean[] isNull = col.isNull;
       for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++) {
         if (isNull[rowIndex])
           dout.addInvalidCol(cIdx);
         else
-          dout.addNumCol(cIdx, timestamp ? (oneColumn[rowIndex]/1000000-ADD_OFFSET) : correctTimeStamp(oneColumn[rowIndex]), 0);
+          dout.addNumCol(cIdx, timestamp ? oneColumn[rowIndex] / 1000000 : correctTimeStamp(oneColumn[rowIndex]), 0);
       }
     }
   }
