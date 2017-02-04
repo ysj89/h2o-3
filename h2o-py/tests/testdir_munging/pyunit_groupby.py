fcaf0f37909b65c12a667fd4cfe638831ba26235
wendycwong <wendycwong@yahoo.com>
2017-02-03 16:32:15 -0800

    PUBDEV-3914.  Add group by docs.  Added get_frame() size check.

--- h2o-py/tests/testdir_munging/pyunit_groupby.py
+++ h2o-py/tests/testdir_munging/pyunit_groupby.py
@@ -4,16 +4,12 @@ sys.path.insert(1,"../../")
 import h2o
 from tests import pyunit_utils
 
-
-
-import pandas as pd
-import numpy as np
-
 def group_by():
     # Connect to a pre-existing cluster
     
 
     h2o_iris = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris_wheader.csv"), na_strings=['NA'])
+    tcount = h2o_iris.nacnt()
     na_handling = ["rm","all"]
 
     print("Running smoke test")
@@ -25,49 +21,61 @@ def group_by():
       grouped.count(na=na)
       temp = grouped.get_frame()    # should get a frame 3 by 2
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 2)
 
       print("Test group_by count, min....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 6)
 
       print("Test group_by count, min, max....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 10)
 
       print("Test group_by count, min, max, mean....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na).mean(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 14)
+
       print("Test group_by count, min, max, mea, var....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na).mean(na=na).var(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 18)
 
       print("Test group_by count, min, max, mea, var, sd....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na).mean(na=na).var(na=na).sd(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 22)
 
       print("Test group_by count, min, max, mea, var, sd, ss....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na).mean(na=na).var(na=na).sd(na=na).ss(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 26)
 
       print("Test group_by count, min, max, mea, var, sd, ss, sum....")
       grouped = h2o_iris.group_by("class")  # setup the Groupby object
       grouped.count(na=na).min(na=na).max(na=na).mean(na=na).var(na=na).sd(na=na).ss(na=na).sum(na=na)
       temp = grouped.get_frame()
       print(grouped.get_frame())
+      AssertGetFrameSize(temp, 3, 30)
 
       sys.stdout.flush()
+
+def AssertGetFrameSize(h2oframe, expectedRows, expectedCols):
+  assert (h2oframe.ncols==expectedCols) and (h2oframe.nrows==expectedRows), "The group_by operation failed."
 
 
 if __name__ == "__main__":
